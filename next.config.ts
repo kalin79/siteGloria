dff9c17aa03f5ff34d636770afb45abcb992d689
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2025-02-15 13:41:43 -0500

    subiendo primera vez

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true, // Habilita modo estricto en React
+  sassOptions: {
+    includePaths: ["./styles"],
+  },
 };
 
 export default nextConfig;
